9fc5e8f735eb287824676a4f88cdf958a65f6f08
Matheus Henrique C Barbosa <matheus.henrique.campos0408@gmail.com>
2025-06-09 21:50:36 -0300

    Update Cadastro.js

--- api_viacep_Js/js/Cadastro.js
+++ api_viacep_Js/js/Cadastro.js
@@ -45,3 +45,50 @@ const pesquisaCep= async()=>{
 
 }
 document.getElementById('cep').addEventListener('focusout',pesquisaCep);
+
+//enviar os dados do formulario para a API rh_tech
+
+const cadastraFuncionario=async()=>{
+    const funcionario={
+        nome:document.getElementById('nome').value,
+        email:document.getElementById('email').value,
+        senha:document.getElementById('senha').value,
+        cep:document.getElementById('cep').value,
+        endereco:document.getElementById('endereco').value,
+        numero:document.getElementById('numero').value,
+        bairro:document.getElementById('bairro').value,
+        cidade:document.getElementById('cidade').value,
+        estado:document.getElementById('estado').value
+    };
+    
+    try {
+        const resposta = await fetch("http://localhost:8080/funcionarios", {
+            method: "POST",
+            headers:{
+                "Content-Type":"Application/json"
+            },
+            body: JSON.stringify(funcionario)
+        });
+
+        if (resposta.ok) {
+            Swal.fire({
+                icon:'success',
+                title: 'sucesso!',
+                text: 'Cadastro realizado com sucesso.'
+            });
+        }else{
+            Swal.fire({
+                icon:'error',
+                title:'Erro!',
+                text: 'Falha ao cadastrar o funcionário. Verifique os dados.'
+            });
+        }
+    } catch (error) {
+        Swal.fire({
+                icon:'error',
+                title:'Erro de conexão!',
+                text: 'Falha ao conectar com à API.'
+            });
+            console.log(error)
+    }
+}
